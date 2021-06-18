3edc6f1a4623c902b9e21f734fae9c945f142683
Jinnan Zhang <vannizhang@gmail.com>
2021-06-18 16:33:14 -0700

    setup webpack and other dev tools

---
+++ webpack.config.js
@@ -0,0 +1,123 @@
+// webpack v4
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports =  (env, options)=> {
+
+    const devMode = options.mode === 'development' ? true : false;
+
+    process.env.NODE_ENV = options.mode;
+
+    return {
+        entry: path.resolve(__dirname, './src/index.tsx'),
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: '[name].[contenthash].js',
+            chunkFilename: '[name].[contenthash].js',
+        },
+        devtool: 'source-map',
+        resolve: {
+            extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    loader: 'babel-loader',
+                },
+                {
+                    test: /\.html$/,
+                    use: [ 
+                        {
+                            loader: "html-loader",
+                            options: { minimize: true }
+                        }
+                    ]
+                },
+                {
+                    test: /\.css$/i,
+                    include: path.resolve(__dirname, 'src'),
+                    use: ['style-loader', 'css-loader', 'postcss-loader'],
+                },
+                { test: /\.(woff|ttf|eot)$/, loader: "file-loader" },
+                { test: /\.(png|jpg|gif|svg)$/,  loader: "file-loader" },
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: devMode ? '[name].css' : '[name].[contenthash].css',
+                chunkFilename: devMode ? '[name].css' : '[name].[contenthash].css',
+            }),
+            // copy static files from public folder to build directory
+            new CopyPlugin({
+                patterns: [
+                    { 
+                        from: "public", 
+                        globOptions: {
+                            ignore: ["**/index.html"],
+                        },
+                        noErrorOnMissing: true
+                    }
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                // inject: false,
+                // hash: true,
+                template: './public/index.html',
+                filename: 'index.html',
+                minify: {
+                    html5                          : true,
+                    collapseWhitespace             : true,
+                    minifyCSS                      : true,
+                    minifyJS                       : true,
+                    minifyURLs                     : false,
+                    removeComments                 : true,
+                    removeEmptyAttributes          : true,
+                    removeOptionalTags             : true,
+                    removeRedundantAttributes      : true,
+                    removeScriptTypeAttributes     : true,
+                    removeStyleLinkTypeAttributese : true,
+                    useShortDoctype                : true
+                }
+            }),
+            !devMode ? new CleanWebpackPlugin() : false,
+            !devMode ? new BundleAnalyzerPlugin() : false
+        ].filter(Boolean),
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    default: false,
+                    vendors: false,
+                    // vendor chunk
+                    vendor: {
+                        // sync + async chunks
+                        chunks: 'all',
+                        name: 'vendor',
+                        // import file path containing node_modules
+                        test: /node_modules/
+                    }
+                }
+            },
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: true,
+                    terserOptions: {
+                        compress: {
+                            drop_console: true,
+                        }
+                    }
+                }), 
+                new OptimizeCSSAssets({})
+            ]
+        }
+    }
+
+};
