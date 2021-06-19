d4cb827571e23821b2ffa8f1cc2720bcf91c9990
Jinnan Zhang <vannizhang@gmail.com>
2021-06-18 17:15:12 -0700

    add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,11 @@
-console.log('hello world')
+import './styles/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1 className='text-4xl text-green-700'>hello</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
