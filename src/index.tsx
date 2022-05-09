ae8767d84c77c9a9d0342f728160b09cb8efb993
Jinnan Zhang <vannizhang@gmail.com>
2022-05-09 14:06:36 -0700

    add dist

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,14 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
     <React.StrictMode>
-        <h1 className='text-4xl text-green-700'>hello</h1>
+        <div className='mt-24 mx-auto text-center'>
+            <h1 className='text-xl mb-4'>Hi There, this site is couple years old and I am renovating now.</h1>
+            <div>
+                <a className='mr-4' href='https://github.com/vannizhang'>My Github Profile</a>
+                <a href='https://www.linkedin.com/in/jinnan-zhang-044a7123/'>My LinkedIn Profile</a>
+            </div>
+        </div>
+        
     </React.StrictMode>,
     document.getElementById('root')
 );
