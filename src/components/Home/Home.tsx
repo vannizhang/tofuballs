d540bdc0331a3633f464ddb7dd6f6d6b8bd9c890
Jinnan Zhang <vannizhang@gmail.com>
2022-05-09 15:27:32 -0700

    update home page

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mt-24 mx-auto text-center'>
+            <h1 className='mb-4'>This site is currently offline becuase I am trying to renovate it a bit...But you can still reach me out via these links below:</h1>
+            <div>
+                <a className='mr-4' href='mailto:vannizhang@gmail.com'>Email</a>
+                <a className='mr-4' href='https://github.com/vannizhang'>Github</a>
+                <a href='https://www.linkedin.com/in/jinnan-zhang-044a7123/'>LinkedIn</a>
+            </div>
+        </div>
+    )
+}
+
+export default Home
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,11 @@ import './styles/index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Home } from './components';
 
 ReactDOM.render(
     <React.StrictMode>
-        <div className='mt-24 mx-auto text-center'>
-            <h1 className='text-xl mb-4'>Hi There, this site is couple years old and I am renovating now.</h1>
-            <div>
-                <a className='mr-4' href='https://github.com/vannizhang'>My Github Profile</a>
-                <a href='https://www.linkedin.com/in/jinnan-zhang-044a7123/'>My LinkedIn Profile</a>
-            </div>
-        </div>
-        
+        <Home />
     </React.StrictMode>,
     document.getElementById('root')
 );
