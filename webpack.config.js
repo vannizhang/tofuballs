0ee74ca36ad31b5d3ae09b9db7f8096b76aa90ec
Jinnan Zhang <vannizhang@gmail.com>
2022-05-09 14:58:43 -0700

    disable BundleAnalyzerPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports =  (env, options)=> {
                 }
             }),
             !devMode ? new CleanWebpackPlugin() : false,
-            !devMode ? new BundleAnalyzerPlugin() : false
+            // !devMode ? new BundleAnalyzerPlugin() : false
         ].filter(Boolean),
         optimization: {
             splitChunks: {
