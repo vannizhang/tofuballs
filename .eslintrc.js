9250120fa562cd6fa644cc9bf66413c7025b0e7a
Jinnan Zhang <vannizhang@gmail.com>
2023-04-07 14:39:36 -0700

    chore: install eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        'prettier' // added
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        'prettier' // added
+    ],
+    "rules": {
+        'prettier/prettier': 'error', // added,
+        "@typescript-eslint/interface-name-prefix": "off"
+    }
+}
